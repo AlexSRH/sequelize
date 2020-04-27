db529e91279ed46938d84eb3eb791451301300aa
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-04-27 20:16:20 -0300

    half class

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+
+const UserController = require('./app/controllers/UserController')
+const AddressController = require('./app/controllers/AddressController')
+
+const routes = Router()
+
+routes.get('/users', UserController.index)
+routes.post('/users', UserController.store)
+
+routes.get('/users/:user_id/addresses', AddressController.index)
+routes.post('/users/:user_id/addresses', AddressController.store)
+
+
+module.exports = routes
